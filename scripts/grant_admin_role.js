9f48591fec361eef4e2af4739d6b1da7a177c9b9
VasilijS <catred@mail2000.ru>
2021-06-21 19:53:07 +0700

    some tests

--- scripts/grant_admin_role.js
+++ scripts/grant_admin_role.js
@@ -1,5 +1,9 @@
 const { parseEther } = require("ethers/utils");
 
+//0x34cBe3A4F129933F53b20F4753Fe781E4f52e451 Tamara
+//0x601a908cC273338357D89a7863E1EcB404DA22f5 Lera
+//0xBC6ae91F55af580B4C0E8c32D7910d00D3dbe54d Vlad
+
 async function main() {
 
     require('dotenv').config();
@@ -8,16 +12,13 @@ async function main() {
     const CitadelFactory = await hre.ethers.getContractFactory("CitadelFactory");
     const CitadelPool = await hre.ethers.getContractFactory("CitadelPool");
 
-    const factory = await CitadelFactory.attach("0xBf5ba43b077A90Ac2b5a59c8b2CD1AfC9866b9c0");
+    const factory = await CitadelFactory.attach("0xC16FcBb4e16F7aC232B1A5439Ac84fD7bB576750");
     let admin_role = await factory.connect(owner).ADMIN_ROLE();
-    await factory.connect(owner).grantRole(admin_role, "0x601a908cC273338357D89a7863E1EcB404DA22f5");
-    await factory.connect(owner).grantRole(admin_role, "0xBC6ae91F55af580B4C0E8c32D7910d00D3dbe54d");
+    await factory.connect(owner).grantRole(admin_role, "0x34cBe3A4F129933F53b20F4753Fe781E4f52e451");
 
     pools = await factory.connect(owner).allPools();
     const pool = await CitadelPool.attach(pools[0].pool);
-
-    await pool.connect(owner).grantRole(admin_role, "0x601a908cC273338357D89a7863E1EcB404DA22f5");
-    await pool.connect(owner).grantRole(admin_role, "0xBC6ae91F55af580B4C0E8c32D7910d00D3dbe54d");
+    await pool.connect(owner).grantRole(admin_role, "0x34cBe3A4F129933F53b20F4753Fe781E4f52e451");
 
 }
 
